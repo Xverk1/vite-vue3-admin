9bbb65773b310c27fe071227c60c9848cf80508b
Xverk1 <xverk1@outlook.com>
2025-02-06 23:43:41 +0800

    完善车站信息

--- src/views/Line/Line.vue
+++ src/views/Line/Line.vue
@@ -1,9 +1,10 @@
-<script lang="ts" setup name="Line">
+<script lang="js" setup name="Line">
 // 脚本
 import { ref } from "vue";
 import axios from "axios";
 
 let isDialogVisible = ref(false);
+let innerVisible = ref(false);
 
 let metroList = ref([]);
 
@@ -15,6 +16,7 @@ let getMetroInfo = async () => {
 
 let targetMetroData = ref({});
 let targetMetroStationsData = ref([]);
+let targetMetroStation = ref({});
 
 getMetroInfo()
   .then((res) => {
@@ -28,7 +30,7 @@ getMetroInfo()
 function seeDetail(metroId) {
   isDialogVisible.value = true;
   let targetMetroId = metroList.value.findIndex(
-    (item: any) => item.id == metroId
+    (item) => item.id == metroId
   );
   targetMetroData.value = metroList.value[targetMetroId];
   targetMetroStationsData.value = targetMetroData.value.stations.filter(
@@ -43,6 +45,12 @@ let dialogScroll = ref();
 function scrollToTop() {
   // console.log("ref获取元素测试", dialogScroll.value);
   dialogScroll.value.setScrollTop(0);
+}
+
+function showStationDetail(data) {
+  console.log("目标车站数据@@@", data);
+  innerVisible.value = true;
+  targetMetroStation.value = data;
 }
 </script>
 
@@ -64,6 +72,53 @@ function scrollToTop() {
     align-center
     @open="scrollToTop"
   >
+    <el-dialog
+      v-model="innerVisible"
+      width="1000"
+      align-center
+      :title="`${targetMetroStation.name}详情`"
+      append-to-body
+    >
+      <el-scrollbar style="height: 70vh">
+        <el-tabs type="card">
+          <el-tab-pane label="站外信息">
+            <el-scrollbar style="height: calc(64.2vh)">
+              <img
+                style="width: 100%; display: block"
+                :src="`https://zzmetro.com${targetMetroStation.thumb}`"
+                alt=""
+              />
+            </el-scrollbar>
+          </el-tab-pane>
+          <el-tab-pane label="出入口信息">
+            <el-scrollbar style="height: calc(64.2vh)">
+              <el-card
+                v-for="entrance in targetMetroStation.passageway"
+                :id="entrance.sitename"
+                style="margin-bottom: 20px"
+              >
+                <p style="font-size: 1.5rem; font-weight: bold">
+                  {{ entrance.sitename }}口
+                </p>
+                <p>街道: {{ entrance.street }}</p>
+                <p>地标: {{ entrance.landmarks }}</p>
+                <p>公交: {{ entrance.bus }}</p>
+              </el-card>
+            </el-scrollbar>
+          </el-tab-pane>
+          <el-tab-pane disabled label="站内服务设施信息">
+            <el-scrollbar style="height: calc(64.2vh)">
+              站内服务设施信息
+            </el-scrollbar>
+          </el-tab-pane>
+          <el-tab-pane disabled label="运营时刻表">
+            <el-scrollbar style="height: calc(64.2vh)">
+              运营时刻表
+            </el-scrollbar>
+          </el-tab-pane>
+        </el-tabs>
+      </el-scrollbar>
+    </el-dialog>
     <el-scrollbar
       style="height: 70vh; position: relative; padding-top: 35px"
       ref="dialogScroll"
@@ -129,16 +184,13 @@ function scrollToTop() {
           />
         </el-table-column>
         <el-table-column align="center" label="">
-          <el-button>车站详情</el-button>
+          <template #default="scope">
+            <el-button @click="showStationDetail(scope.row)"
+              >车站详情</el-button
+            >
+          </template>
         </el-table-column>
       </el-table>
-
-      <!-- <el-table :data="targetMetroData.stations">
-        <el-table-column prop="id" label="ID" />
-        <el-table-column prop="name" label="Name" />
-        <el-table-column prop="address" label="Address" />
-      </el-table> -->
-      <!-- {{ targetMetroData }} -->
     </el-scrollbar>
   </el-dialog>
 </template>
