82e21cd7f6cefe5fd2239498a4edae1358e9297b
Xverk1 <xverk1@outlook.com>
2025-02-13 21:23:40 +0800

    把路由模式修改为Hash模式

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import adminRoutes from "./modules/admin.ts";
 const routes = [
   {
@@ -9,6 +9,6 @@ const routes = [
 ];
 
 export default createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
